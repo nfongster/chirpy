4ee30cf3368f110c8b2dd9306fbb9cf09a668916
Nick <nickfong@2017-041.rz.local>
2025-08-12 09:11:33 -0700

    add middleware for counting home page hits

--- main.go
+++ main.go
@@ -4,19 +4,46 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	f := func(wrt http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(wrt, req)
+	}
+	return http.HandlerFunc(f)
+}
 
 func main() {
 	fmt.Println("Starting chirpy server...")
 
 	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
 	mux.HandleFunc("/healthz", func(wrt http.ResponseWriter, _ *http.Request) {
 		wrt.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		wrt.WriteHeader(200)
 		wrt.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("/metrics", func(wrt http.ResponseWriter, _ *http.Request) {
+		wrt.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		wrt.WriteHeader(200)
+		hits := apiCfg.fileserverHits.Load()
+		fmt.Fprintf(wrt, "Hits: %d", hits)
+	})
+
+	mux.HandleFunc("/reset", func(wrt http.ResponseWriter, _ *http.Request) {
+		wrt.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		wrt.WriteHeader(200)
+		apiCfg.fileserverHits.Swap(0)
 	})
 
 	server := &http.Server{
