24190f3236b67e03de39d8beccb492101b80f6a6
Nick <nickfong@2017-041.rz.local>
2025-08-12 20:40:01 -0700

    add endpoint for validating chirp lengths

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -17,6 +18,22 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 		next.ServeHTTP(wrt, req)
 	}
 	return http.HandlerFunc(f)
+}
+
+func getValidateChirpResponse(chirpLength int) (int, any) {
+	if chirpLength > 140 {
+		return 400, struct {
+			Error string `json:"error"`
+		}{
+			Error: "Chirp is too long",
+		}
+	} else {
+		return 200, struct {
+			Valid bool `json:"valid"`
+		}{
+			Valid: true,
+		}
+	}
 }
 
 func main() {
@@ -45,6 +62,34 @@ func main() {
 		wrt.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		wrt.WriteHeader(200)
 		apiCfg.fileserverHits.Swap(0)
+	})
+
+	mux.HandleFunc("POST /api/validate_chirp", func(wrt http.ResponseWriter, req *http.Request) {
+		// Handle request
+		type parameters struct {
+			Body string `json:"body"`
+		}
+
+		decoder := json.NewDecoder(req.Body)
+		params := parameters{}
+		if err := decoder.Decode(&params); err != nil {
+			fmt.Printf("Error decoding parameters: %s\n", err)
+			wrt.WriteHeader(500)
+			return
+		}
+
+		// Write response
+		code, jsonMessage := getValidateChirpResponse(len(params.Body))
+		dat, err := json.Marshal(jsonMessage)
+		if err != nil {
+			fmt.Printf("Error marshalling JSON: %s\n", err)
+			wrt.WriteHeader(500)
+			return
+		}
+
+		wrt.Header().Set("Content-Type", "application/json")
+		wrt.WriteHeader(code)
+		wrt.Write(dat)
 	})
 
 	server := &http.Server{
